47face4d482d28ca4e8ea8151e6342dc14fc3668
imane <madouimimane@gmail.com>
2025-04-06 03:39:12 +0100

    refactor(auth): remove unnecessary code

--- authentication/src/routes/index.ts
+++ authentication/src/routes/index.ts
@@ -1,3 +1,2 @@
 export * from './signup';
-
 export { default as signUpRouter } from './signup';
--- authentication/src/routes/utils/index.ts
+++
@@ -1,9 +0,0 @@
-import express, { Request, Response, NextFunction } from 'express';
-
-export const handleMethodNotAllowed = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  res.sendStatus(405);
-};
